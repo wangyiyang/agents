6a69b3a3851b7cedefd188ff62c37ab75d50a532
wangyiyang <wangyiyang@users.noreply.github.com>
2024-01-23 00:16:06 +0000

    ✅ Ready to clone and code.

--- agents/app.py
+++ agents/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+agents API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="agents",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://agents.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "wangyiyang",
+        "url": "http://agents.com/contact/",
+        "email": "wangyiyang@agents.com",
     },
     license_info={
         "name": "The Unlicense",
